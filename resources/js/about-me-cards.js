6ab06ada6f7114db547eec7ec81d5d954775c2de
Martins Kepits <martinskepitslv@gmail.com>
2024-03-22 18:55:19 -0400

    Create that about me cards on mobile have same height

--- resources/js/about-me-cards.js
+++ resources/js/about-me-cards.js
@@ -1,16 +1,23 @@
 $(document).ready(function () {
     const screenWidth = $(window).width();
+    const aboutCards = $(".about-me-cards .about-card");
+    const aboutCardsTitles = $(".about-me-cards .card-title");
 
-    if (screenWidth >= 991) {
-        const cards = $(".about-me-cards .card-title");
-        const tallestCardTitle = Math.max(
-            ...cards
+    const getMaxHeight = (elements) => {
+        return Math.max(
+            ...elements
                 .map(function () {
                     return $(this).height();
                 })
                 .get()
         );
+    };
 
-        cards.height(tallestCardTitle);
+    if (screenWidth >= 991) {
+        const tallestCardTitle = getMaxHeight(aboutCardsTitles);
+        aboutCardsTitles.height(tallestCardTitle);
+    } else {
+        const tallestCard = getMaxHeight(aboutCards);
+        aboutCards.height(tallestCard);
     }
 });
