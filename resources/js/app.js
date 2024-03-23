36a33e241c32081270e5b3d819a2c760fc0e5bce
Martins Kepits <martinskepitslv@gmail.com>
2024-03-22 20:13:41 -0400

    Add to project Font Awesome icons

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,4 +1,11 @@
 import "./bootstrap";
+
+import "@fortawesome/fontawesome-free/scss/fontawesome.scss";
+import "@fortawesome/fontawesome-free/scss/brands.scss";
+import "@fortawesome/fontawesome-free/scss/regular.scss";
+import "@fortawesome/fontawesome-free/scss/solid.scss";
+import "@fortawesome/fontawesome-free/scss/v4-shims.scss";
+
 import "./navbar";
 import "./hero-bg-animation";
 import "./about-me-cards";
