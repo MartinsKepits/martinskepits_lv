abe41bd591e5de18f359c2f9c359d23559ea280e
Martins Kepits <martinskepitslv@gmail.com>
2024-04-19 20:49:02 -0400

    Code Refactor

--- routes/web.php
+++ routes/web.php
@@ -3,9 +3,6 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\MailController;
 
-Route::get('/', function () {
-    return view('home');
-})->name('home');
-
+Route::view('/', 'home')->name('home');
 Route::get('/contact', [MailController::class, 'mailForm'])->name('contact');
 Route::post('/send-mail', [MailController::class, 'mailData'])->name('send_mail');
